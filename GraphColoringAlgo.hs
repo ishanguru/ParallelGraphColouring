b8c4cfc7cf2b9ded1acb69bdaaeb4e743c2c8345
Haneen Mohammed <hamohammed.sa@gmail.com>
2019-12-17 00:09:05 -0500

    add graph coloring using independent set algo

--- GraphColoringAlgo.hs
+++ GraphColoringAlgo.hs
@@ -1,11 +1,14 @@
 module GraphColoringAlgo
 ( colorGraph,
-  colorAGraph
+  colorAGraph,
+  colorIndependentPar,
+  colorIndependent
 ) where
 
 import Utils
 
 import qualified Data.Map as Map
+import Control.Parallel.Strategies (rpar, rseq, runEval, parListChunk, using)
 
 -- assigns a colour to each node in the graph
 -- e.g. colorGraph (Map.keys g) [1,2,3,4] g
@@ -30,4 +33,52 @@ colorAGraph graph_file colours outFolder inFolder = do
               writeToFile output outFile
               return $ "done coloring " ++ graph_file
 
+-- g = fromList [("A",(["B","C"],0)),("B",(["A","C","D","E","F"],0)),
+-- ("C",(["A","B","D"],0)),("D",(["B","C","E"],0)),("E",(["B","D","F"],0)),("F",(["B","E"],0))]
+-- U = ["A","B","C","D","E","F"]
+-- v = "A"
+-- I = ["A"]
+-- X = ["A", "B", "C"]
+-- U = U - X = ["E", "F"]
+-- InducedGraph g U = [("E", (["F"], 0)), ("F", (["E"], 0))]
+inducedGraph :: Graph -> [Node] -> Graph
+inducedGraph g nodes =  Map.fromList ( map (\x -> (x, (adj x, 0))) nodes `using` parListChunk 2000 rpar )
+--inducedGraph g nodes =  Map.fromList ( map (\x -> (x, (adj x, 0))) nodes )
+                            where adj = (\nx -> filter (\y -> y `elem` nodes) $ getNeighbors nx g)
 
+-- g = fromList [("A",(["B","C"],0)),("B",(["A","C","D","E","F"],0)),
+independentSet :: Graph -> Graph -> [Node] -> [Node] -> [Node]
+independentSet _ _ [] i = i
+independentSet g ig u@(x:_) i | length (Map.keys ig) == 0 = i
+                               | otherwise = independentSet g ig_new u_new i_new
+                                             where i_new = x : i
+                                                   u_new = filter (\y -> y `notElem` (x: getNeighbors x g)) u
+                                                   ig_new = inducedGraph g  u_new
+                                                  
+colorIndependent :: Graph -> Graph -> [Node] -> [Color] -> Maybe Graph
+colorIndependent g _ [] _ = Just g
+colorIndependent _ _ _ [] = Nothing
+colorIndependent g ig u (c:cs) | length (Map.keys ig) == 0 = Just g
+                               | otherwise = colorIndependent colored_g ig_new u_new cs
+                                            where i_new = independentSet ig ig u_nodes [] 
+                                                  u_nodes = Map.keys ig
+                                                  u_new = filter (\y -> y `notElem` i_new) u
+                                                  ig_new = inducedGraph g  u_new
+                                                  colored_g = colorNodes g i_new c
+colorNodes :: Graph -> [Node] -> Color -> Graph
+colorNodes g [] _ = g
+colorNodes g nodes c = Map.union (fst pr) (Map.mapWithKey (\_ x -> (fst x, c)) (snd pr))
+                       where pr = Map.partitionWithKey (\k _ -> k `notElem` nodes) g
+--colorNodes g (x:xs) c = colorNodes (setColor g x c) xs c
+
+colorIndependentPar :: Graph -> Graph -> [Node] -> [Color] -> Maybe Graph
+colorIndependentPar g _ _ [] = Just g
+colorIndependentPar g _ [] _ = Just g
+colorIndependentPar g ig u (c:cs) | length (Map.keys ig) == 0 = Just g
+                               | otherwise = runEval $ do 
+                                            i_new <- rseq $ independentSet ig ig u_nodes [] 
+                                            u_new <- rpar $ filter (\y -> y `notElem` i_new) u
+                                            colored_g <- rpar $ colorNodes g i_new c
+                                            ig_new <- rpar $ inducedGraph g u_new
+                                            return $ colorIndependentPar colored_g ig_new u_new cs
+                                            where u_nodes = Map.keys ig
--- graph_colouring.hs
+++ graph_colouring.hs
@@ -5,6 +5,7 @@ import GraphColoringAlgo
 import System.Exit(die)
 import System.Environment(getArgs, getProgName)
 
+import qualified Data.Map as Map
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Par.Combinator (parMapM)
 import Control.Monad.Par.IO (runParIO)
@@ -28,6 +29,23 @@ main = do
           filepaths <- filter isValidFile <$> getDirectoryContents inFolder
           responses <- runParIO $ parMapM (\f -> liftIO $ colorAGraph f colours outFolder inFolder) filepaths
           mapM_ putStrLn responses
+        "IndepSet" -> do
+          -- following algo in:
+          -- http://www.ii.uib.no/~assefaw/pub/coloring/thesis.pdf
+          -- an order of mag slower than colorGraph algorithm
+          g <- readGraphFile graph_file
+          let graph_file_name = last $ wordsWhen (=='/') graph_file
+          let outFile = outFolder ++ "/" ++ graph_file_name ++ "_out"
+          let output = checkValidColored $ colorIndependent g g (Map.keys g) [1..colours]
+          response output graph_file
+          writeToFile output outFile
+        "IndepSetPar" -> do
+          g <- readGraphFile graph_file
+          let graph_file_name = last $ wordsWhen (=='/') graph_file
+          let outFile = outFolder ++ "/" ++ graph_file_name ++ "_out"
+          let output = checkValidColored $ colorIndependentPar g g (Map.keys g) [1..colours]
+          response output graph_file
+          writeToFile output outFile
         _ -> do 
           die $ "Usage: " ++ pn ++ " <graph-{file/folder}name/> <number-of-colors> <algo: seq or par> <output-folder>"
 
