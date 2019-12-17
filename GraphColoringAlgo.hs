9f39a526d6716e69c1ffd0287c84bac710313cca
Haneen Mohammed <hamohammed.sa@gmail.com>
2019-12-17 13:36:05 -0500

    rename & test independent set

--- GraphColoringAlgo.hs
+++ GraphColoringAlgo.hs
@@ -1,26 +1,25 @@
 module GraphColoringAlgo
-( colorGraph,
+( backtracking,
   colorAGraph,
-  colorIndependentPar,
   colorIndependent
 ) where
 
 import Utils
 
 import qualified Data.Map as Map
-import Control.Parallel.Strategies (rpar, rseq, runEval, parListChunk, using)
+import Control.Parallel.Strategies (rpar, rseq, runEval, parListChunk, using, parMap, parBuffer)
 
 -- assigns a colour to each node in the graph
--- e.g. colorGraph (Map.keys g) [1,2,3,4] g
-colorGraph :: [Node] -> [Color] -> [Color] -> Graph -> Maybe Graph
-colorGraph _ [] _ g = Just g
-colorGraph [] _ _ g = Just g
-colorGraph _ _ [] _ = Nothing
-colorGraph nodes@(n:ns) colors (c:cs) g
-      | validColor n c g = case (colorGraph ns colors colors $ setColor g n c) of
+-- e.g. backtracking (Map.keys g) [1,2,3,4] g
+backtracking :: [Node] -> [Color] -> [Color] -> Graph -> Maybe Graph
+backtracking _ [] _ g = Just g
+backtracking [] _ _ g = Just g
+backtracking _ _ [] _ = Nothing
+backtracking nodes@(n:ns) colors (c:cs) g
+      | validColor n c g = case (backtracking ns colors colors $ setColor g n c) of
                               Just gout -> Just gout
-                              Nothing -> colorGraph nodes colors cs g
-      | otherwise = colorGraph nodes colors cs g
+                              Nothing -> backtracking nodes colors cs g
+      | otherwise = backtracking nodes colors cs g
 
 colorAGraph :: FilePath -> Color -> String -> String -> IO String
 colorAGraph graph_file colours outFolder inFolder = do
@@ -28,7 +27,7 @@ colorAGraph graph_file colours outFolder inFolder = do
               let outFile = outFolder ++ "/" ++ graph_file_name ++ "_out"
               g <- readGraphFile $ inFolder ++ graph_file
               putStrLn ("coloring " ++ graph_file ++ " .. ")
-              let output =  checkValidColored $  colorGraph (Map.keys g) [1..colours] [1..colours] g
+              let output =  checkValidColored $  backtracking (Map.keys g) [1..colours] [1..colours] g
               response output graph_file
               writeToFile output outFile
               return $ "done coloring " ++ graph_file
@@ -42,8 +41,9 @@ colorAGraph graph_file colours outFolder inFolder = do
 -- U = U - X = ["E", "F"]
 -- InducedGraph g U = [("E", (["F"], 0)), ("F", (["E"], 0))]
 inducedGraph :: Graph -> [Node] -> Graph
-inducedGraph g nodes =  Map.fromList ( map (\x -> (x, (adj x, 0))) nodes `using` parListChunk 2000 rpar )
---inducedGraph g nodes =  Map.fromList ( map (\x -> (x, (adj x, 0))) nodes )
+--inducedGraph g nodes =  Map.fromList ( map (\x -> (x, (adj x, 0))) nodes `using` parBuffer 100 rseq )
+--inducedGraph g nodes =  Map.fromList ( map (\x -> (x, (adj x, 0))) nodes `using` parListChunk 100 rseq )
+inducedGraph g nodes =  Map.fromList ( parMap rseq (\x -> (x, (adj x, 0))) nodes )
                             where adj = (\nx -> filter (\y -> y `elem` nodes) $ getNeighbors nx g)
 
 -- g = fromList [("A",(["B","C"],0)),("B",(["A","C","D","E","F"],0)),
@@ -55,30 +55,20 @@ independentSet g ig u@(x:_) i | length (Map.keys ig) == 0 = i
                                                    u_new = filter (\y -> y `notElem` (x: getNeighbors x g)) u
                                                    ig_new = inducedGraph g  u_new
                                                   
-colorIndependent :: Graph -> Graph -> [Node] -> [Color] -> Maybe Graph
-colorIndependent g _ [] _ = Just g
-colorIndependent _ _ _ [] = Nothing
-colorIndependent g ig u (c:cs) | length (Map.keys ig) == 0 = Just g
-                               | otherwise = colorIndependent colored_g ig_new u_new cs
-                                            where i_new = independentSet ig ig u_nodes [] 
-                                                  u_nodes = Map.keys ig
-                                                  u_new = filter (\y -> y `notElem` i_new) u
-                                                  ig_new = inducedGraph g  u_new
-                                                  colored_g = colorNodes g i_new c
 colorNodes :: Graph -> [Node] -> Color -> Graph
 colorNodes g [] _ = g
 colorNodes g nodes c = Map.union (fst pr) (Map.mapWithKey (\_ x -> (fst x, c)) (snd pr))
                        where pr = Map.partitionWithKey (\k _ -> k `notElem` nodes) g
 --colorNodes g (x:xs) c = colorNodes (setColor g x c) xs c
 
-colorIndependentPar :: Graph -> Graph -> [Node] -> [Color] -> Maybe Graph
-colorIndependentPar g _ _ [] = Just g
-colorIndependentPar g _ [] _ = Just g
-colorIndependentPar g ig u (c:cs) | length (Map.keys ig) == 0 = Just g
+colorIndependent :: Graph -> Graph -> [Node] -> [Color] -> Maybe Graph
+colorIndependent g _ _ [] = Just g
+colorIndependent g _ [] _ = Just g
+colorIndependent g ig u (c:cs) | length (Map.keys ig) == 0 = Just g
                                | otherwise = runEval $ do 
                                             i_new <- rseq $ independentSet ig ig u_nodes [] 
                                             u_new <- rpar $ filter (\y -> y `notElem` i_new) u
                                             colored_g <- rpar $ colorNodes g i_new c
                                             ig_new <- rpar $ inducedGraph g u_new
-                                            return $ colorIndependentPar colored_g ig_new u_new cs
+                                            return $ colorIndependent colored_g ig_new u_new cs
                                             where u_nodes = Map.keys ig
--- graph_colouring.hs
+++ graph_colouring.hs
@@ -19,12 +19,9 @@ main = do
     [graph_file, number_colours, algo, outFolder] -> do
       let colours = read number_colours
       case algo of
-        "seq" -> do 
-          msg <- colorAGraph graph_file colours outFolder ""
-          putStrLn msg
         "par" -> do 
           putStrLn "not implemented"
-        "all" -> do
+        "backtracking" -> do
           let inFolder = graph_file
           filepaths <- filter isValidFile <$> getDirectoryContents inFolder
           responses <- runParIO $ parMapM (\f -> liftIO $ colorAGraph f colours outFolder inFolder) filepaths
@@ -32,18 +29,11 @@ main = do
         "IndepSet" -> do
           -- following algo in:
           -- http://www.ii.uib.no/~assefaw/pub/coloring/thesis.pdf
-          -- an order of mag slower than colorGraph algorithm
+          -- an order of mag slower than backtracking algorithm
           g <- readGraphFile graph_file
           let graph_file_name = last $ wordsWhen (=='/') graph_file
           let outFile = outFolder ++ "/" ++ graph_file_name ++ "_out"
           let output = checkValidColored $ colorIndependent g g (Map.keys g) [1..colours]
-          response output graph_file
-          writeToFile output outFile
-        "IndepSetPar" -> do
-          g <- readGraphFile graph_file
-          let graph_file_name = last $ wordsWhen (=='/') graph_file
-          let outFile = outFolder ++ "/" ++ graph_file_name ++ "_out"
-          let output = checkValidColored $ colorIndependentPar g g (Map.keys g) [1..colours]
           response output graph_file
           writeToFile output outFile
         _ -> do 
